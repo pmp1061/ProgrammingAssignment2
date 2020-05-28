7d0646b4443d4ce79b2c8e192d7b384e9370d494
pmp1061 <pradeep.patil@vpkbiet.org>
2020-05-28 18:32:04 +0530

    Assignment submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 
+##  makeCacheMatrix function set and get the matrix,also set and get inverse of the matrix
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  
+  matrix_inverse <- NULL
+  
+  
+  # set and get methods for given matrix
+  getMatrix=function() {
+    x
+  }
+  
+  setMatrix <- function(y) {
+    x <<- y
+    matrix_inverse <<- NULL
+  }
+  
+  
+  # set and get methods for inverse matrix
+  
+  setMatrixInverse <- function( inverse) {
+    matrix_inverse <- inverse
+  }
+  
+  getMatrixInverse <- function() {
+    matrix_inverse
+  }
+  
+  # list of functions
+  list(setMatrix = setMatrix, getMatrix = getMatrix ,setMatrixInverse = setMatrixInverse, getMatrixInverse =getMatrixInverse )
+  
+  
+  
+  
 
 }
 
 
-## Write a short comment describing this function
+## 
+##  cacheSolve function computes the inverse of the matrix.
+##     if the inverse is already calculatedthen it reterives from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  cmatrixinverse <- x$getMatrixInverse()
+  
+  if (!is.null(cmatrixinverse)) {
+    return(cmatrixinverse)
+  }
+  
+  data <- x$getMatrix()
+  matrixInverse <- solve(data, ...)
+  x$setMatrixInverse(matrixInverse)
+  matrixInverse
+  
+  
 }
